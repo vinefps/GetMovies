77c084b3e4e8331c4376df99b5c008dc5099a222
Vinicius de Oliveira <vinicius_os95@hotmail.com>
2024-07-23 00:15:18 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const movies = require("./routes/movieRoute");
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+require('dotenv').config(); // Terei acesso em .env em todo app
 
 const app = express();
 const port = 3000;
+console.log('API_MOVIE_KEY:', process.env.API_MOVIE_KEY);
 
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('mustache', mustacheExpress());
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(movies);
 
-app.use('/movies',movies)
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
--- src/routes/movieRoute.js
+++ src/routes/movieRoute.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-require('dotenv').config();
 
-// Exemplo de rota
-app.get('/', async (req, res) => {
-    const query = req.query.query;
-    
-    if (!query) {
-        return res.status(400).send({ error: 'Query parameter is required' });
-    }
+router.get('/', async (req, res) => {
+    const query = 'Batman';
 
     try {
         const response = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
@@ -17,14 +11,13 @@ app.get('/', async (req, res) => {
                 api_key: process.env.API_MOVIE_KEY,
                 query: query
             },
-            
         });
         res.json(response.data);
     } catch (error) {
         res.status(500).send({ error: 'Error fetching data from TMDb' });
     }
+    // res.render('index');
 });
 
 module.exports = router;
-
 
